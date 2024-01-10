e98a31bf09f35dbcc3a977df6bebc854b42652e2
Vitalii Tungrikov <tungrikov@mail.ru>
2024-01-11 00:56:52 +0300

    Создал вариант для 6 спринта, не работает алерт.

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by admin on 10.01.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by admin on 10.01.2024.
+//
+
+import Foundation
